7d5fa5bc4767fb821d0adf763d1f07efd8fe773e
baocq <baocq@fenbi.com>
2020-07-14 19:04:23 +0800

    TMP

--- base/mini_check_update.sh
+++ base/mini_check_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
-_REMOTE_URL=`git remote -v`
+_REMOTE_URL=`git ls-remote --get-url origin`
+_LATEST_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD`
 echo $_REMOTE_URL
+echo $_LATEST_COMMIT
 
 
