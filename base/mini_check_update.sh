9674610bfb307eba2062a668bebf2c169b99b847
baocq <baocq@fenbi.com>
2020-07-15 00:08:57 +0800

    TMP

--- base/mini_check_update.sh
+++ base/mini_check_update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 
 _REMOTE_URL=`git ls-remote --get-url origin`
-_LATEST_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD`
-echo $_REMOTE_URL
-echo $_LATEST_COMMIT
+_LATEST_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD | xargs -n 1 | head -n 1`
+_CURRENT_COMMIT=`git rev-parse HEAD`
 
-
+if [ $_LATEST_COMMIT == $_CURRENT_COMMIT ]; then
+  echo "Repository updated!"
+else
+  echo "Repository Same!"
+fi
