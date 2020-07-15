b606ac4cf737be67a4e016728a6b163923da4f7a
baocq <baocq@fenbi.com>
2020-07-15 22:04:15 +0800

    TMP

--- base/mini_check_update.sh
+++ base/mini_check_update.sh
@@ -4,24 +4,14 @@ _REMOTE_URL=`git ls-remote --get-url origin`
 _REMOTE_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD | xargs -n 1 | head -n 1`
 _CURRENT_COMMIT=`git rev-parse HEAD`
 
-function _git_is_ancestor() 
-{
-  if git merge-base --is-ancestor $1 $2; then
-    echo 'ancestor'
-  elif git merge-base --is-ancestor $2 $1; then
-    echo 'descendant'
-  else
-    echo 'Unrelated'
-  fi
-}
-
-_RELATION=`_git_is_ancestor $_REMOTE_COMMIT $_CURRENT_COMMIT`
 if [[ $_REMOTE_COMMIT == $_CURRENT_COMMIT ]];then
-  echo "Same Commit, do not need to update..."
-elif [[ $_RELATION == "ancestor" ]];then
-  echo "Need to update"
-elif [[ $_RELATION == "descendant" ]];then
-  echo "Descendant, do not need to update..."
+  # Same commit, do not need to update
+  echo "Same Commit, do not need to update"
+elif git merge-base --is-ancestor $_REMOTE_COMMIT $_CURRENT_COMMIT;then
+  # Local commit is based on remote commit
+  echo "Local commit is based on remote commit, do not need to update"
 else
-  echo "Unrelated"
+  # Local branch is behind remote commit, and have new unmerged commit
+  echo "Local branch is behind remote branch, and have unsubmitted local commit,
+  need to update"
 fi
---
+++ mini.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function _mini_usage() {
+  echo "usage: mini [--help]"
+  echo "            <command> [<args>]"
+  echo ""
+  echo "These are common Mini commands used in various situations:"
+  echo ""
+  echo "start a iOS environment"
+  echo "   setup"
+  echo ""
+  echo "'mini help -a' and 'mini help -g' list available subcommands and some
+  concept guides."
+  echo ""
+}
+
+function mini() {
+  if [ $# -eq 0 ]; then
+    _mini_usage;
+    exit 1
+  fi
+
+  while getopts "i:b:" OPT; do
+    case $OPT in
+      i) echo "INIT";;
+      b) TEST2="$OPTARG";;
+      ?) _mini_usage;;
+    esac 
+  done
+}
+
+mini $*
