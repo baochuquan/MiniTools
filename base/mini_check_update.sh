44d099d9dd07c277a6af8f387211675ff0160587
baocq <baocq@fenbi.com>
2020-07-15 09:23:47 +0800

    TMP

--- base/mini_check_update.sh
+++ base/mini_check_update.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 
 _REMOTE_URL=`git ls-remote --get-url origin`
-_LATEST_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD | xargs -n 1 | head -n 1`
+_REMOTE_COMMIT=`git ls-remote ${_REMOTE_URL} HEAD | xargs -n 1 | head -n 1`
 _CURRENT_COMMIT=`git rev-parse HEAD`
 
-if [ $_LATEST_COMMIT == $_CURRENT_COMMIT ]; then
-  echo "Repository updated!"
-else
-  echo "Repository Same!"
+function _git_is_ancestor() 
+{
+  if git merge-base --is-ancestor $1 $2; then
+    echo 'ancestor'
+  elif git merge-base --is-ancestor $2 $1; then
+    echo 'descendant'
+  else
+    echo 'Unrelated'
+  fi
+}
+
+_RELATION=`_git_is_ancestor $_REMOTE_COMMIT $_CURRENT_COMMIT`
+
+if [[ $_RELATION == "ancestor" ]];then
+  echo "Need update"
 fi
+
+
